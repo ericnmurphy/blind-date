0cbaa3aa9043548e09b27ed5e4a7cd82ea93296f
Eric Murphy <ericnmurphy@gmail.com>
2018-09-02 10:41:21 +0700

    Added user sorting

--- client/src/Users/Users.js
+++ client/src/Users/Users.js
@@ -3,12 +3,13 @@ import axios from "axios";
 
 export default class Users extends Component {
   state = {
-    users: []
+    users: [],
+    value: "",
+    desc: true
   };
 
   componentDidMount() {
     axios.get("/api/users", {}).then(res => {
-      console.log(res.data);
       this.setState({
         authenticated: true,
         users: res.data
@@ -25,6 +26,7 @@ export default class Users extends Component {
   };
 
   render() {
+    const value = this.state.value;
     return (
       <div className="users-table">
         {this.state.authenticated === undefined && <h3>Loading...</h3>}
@@ -32,25 +34,204 @@ export default class Users extends Component {
           <table>
             <thead>
               <tr>
-                <th>Email</th>
-                <th>Name</th>
-                <th>Profession</th>
-                <th>Company</th>
-                <th>Website</th>
-                <th>Association</th>
-                <th>Gender</th>
-                <th>Age</th>
-                <th>Socioeconomic</th>
-                <th>Height</th>
+                <th
+                  className={
+                    this.state.value === "email" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "email"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "email" });
+                  }}
+                >
+                  Email
+                </th>
+                <th
+                  className={
+                    this.state.value === "lastName" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "lastName"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "lastName" });
+                  }}
+                >
+                  Name
+                </th>
+                <th
+                  className={
+                    this.state.value === "profession" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "profession"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "profession" });
+                  }}
+                >
+                  Profession
+                </th>
+                <th
+                  className={
+                    this.state.value === "company" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "company"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "company" });
+                  }}
+                >
+                  Company
+                </th>
+                <th
+                  className={
+                    this.state.value === "website" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "website"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "website" });
+                  }}
+                >
+                  Website
+                </th>
+                <th
+                  className={
+                    this.state.value === "association" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "association"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "association" });
+                  }}
+                >
+                  Association
+                </th>
+                <th
+                  className={
+                    this.state.value === "gender" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "gender"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "gender" });
+                  }}
+                >
+                  Gender
+                </th>
+                <th
+                  className={
+                    this.state.value === "age" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "age"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "age" });
+                  }}
+                >
+                  Age
+                </th>
+                <th
+                  className={
+                    this.state.value === "socioeconomic" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "socioeconomic"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "socioeconomic" });
+                  }}
+                >
+                  Socioeconomic
+                </th>
+                <th
+                  className={
+                    this.state.value === "height" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "height"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "height" });
+                  }}
+                >
+                  Height
+                </th>
                 <th>Adjectives</th>
-                <th>Best Quality</th>
-                <th>Beard</th>
-                <th>Haunt</th>
+                <th
+                  className={
+                    this.state.value === "bestQuality" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "bestQuality"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "bestQuality" });
+                  }}
+                >
+                  Best Quality
+                </th>
+                <th
+                  className={
+                    this.state.value === "beard" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "beard"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "beard" });
+                  }}
+                >
+                  Beard
+                </th>
+                <th
+                  className={
+                    this.state.value === "haunt" &&
+                    (this.state.desc ? "sort-desc" : "sort-asc")
+                  }
+                  onClick={() => {
+                    this.state.value === "haunt"
+                      ? this.setState(prevState => ({ desc: !prevState.desc }))
+                      : this.setState({ desc: true });
+                    this.setState({ value: "haunt" });
+                  }}
+                >
+                  Haunt
+                </th>
                 <th>Referrer</th>
               </tr>
             </thead>
             <tbody>
               {this.state.users
+                .sort((a, b) => {
+                  let comparison = 0;
+                  if (a[value] < b[value]) {
+                    comparison = -1;
+                  }
+                  if (a[value] > b[value]) {
+                    comparison = 1;
+                  }
+                  return !this.state.desc ? comparison * -1 : comparison;
+                })
                 .filter(user => {
                   return user.firstName;
                 })
