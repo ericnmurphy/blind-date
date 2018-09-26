26640601b8bb92c24f39d0ffeda4e37a6a7c2024
Eric Murphy <ericnmurphy@gmail.com>
2018-09-26 17:02:33 +0700

    Added authentication routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Link, Redirect } from "react-router-dom";
+import { Route, Link, Redirect, Switch } from "react-router-dom";
 import ReferralForm from "./ReferralForm/ReferralForm";
 import MatchList from "./Matches/MatchList";
 import SentMatchList from "./Matches/SentMatchList";
@@ -21,14 +21,53 @@ const fakeAuth = {
   }
 };
 
-const Public = () => <h3>Public</h3>;
-const Protected = () => <h3>Protected</h3>;
-
 class Login extends Component {
+  state = { redirectToReferrer: false };
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState(() => ({
+        redirectToReferrer: true
+      }));
+    });
+  };
+
   render() {
-    return <div>LOGIN</div>;
+    const { redirectToReferrer } = this.state;
+    const { from } = this.props.location.state || {
+      from: { pathname: "/admin" }
+    };
+
+    if (redirectToReferrer === true) {
+      return <Redirect to={from} />;
+    }
+
+    return (
+      <div>
+        <p>Please log in.</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    );
   }
 }
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      fakeAuth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
 
 class Admin extends Component {
   render() {
@@ -36,16 +75,16 @@ class Admin extends Component {
       <div className="navigation">
         <ul>
           <li>
-            <Link to="/admin/invite">Invite ></Link>
+            <Link to="/a/invite">Invite ></Link>
           </li>
           <li>
-            <Link to="/admin/users">Users ></Link>
+            <Link to="/a/users">Users ></Link>
           </li>
           <li>
-            <Link to="/admin/matches">Matches ></Link>
+            <Link to="/a/matches">Matches ></Link>
           </li>
           <li>
-            <Link to="/admin/matches/sent">Sent Matches ></Link>
+            <Link to="/a/matches/sent">Sent Matches ></Link>
           </li>
         </ul>
       </div>
@@ -57,14 +96,17 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <Route exact path="/:id" component={ReferralForm} />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route path="/a" component={Admin} />
+          <Route exact path="/:id" component={ReferralForm} />
+        </Switch>
         <Route exact path="/invite/:id" component={InviteFriend} />
         <Route exact path="/disable/:id" component={Disable} />
-        <Route path="/admin" component={Admin} />
-        <Route exact path="/admin/invite" component={Invite} />
-        <Route exact path="/admin/matches" component={MatchList} />
-        <Route exact path="/admin/matches/sent" component={SentMatchList} />
-        <Route exact path="/admin/users" component={Users} />
+        <PrivateRoute exact path="/a/invite" component={Invite} />
+        <Route exact path="/a/matches" component={MatchList} />
+        <Route exact path="/a/matches/sent" component={SentMatchList} />
+        <Route exact path="/a/users" component={Users} />
       </div>
     );
   }
